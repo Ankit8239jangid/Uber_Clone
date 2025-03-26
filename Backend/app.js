7e62247eace01ab0e729da45887be23218b1e062
ankit <ankitjangid6375@gmail.com>
2025-03-26 07:01:37 -0400

    fixing production bug

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: '*', // Allow requests from any origin
+    origin: 'https://uber-by-ankit.vercel.app', // Allow requests from your frontend
     credentials: true, // Allow cookies & authorization headers
 }));
 app.use(express.json());
